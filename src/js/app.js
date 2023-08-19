355df948e211ad3ee56d5f65cabaed6fac79f9e6
Rahat Morshed Nabil <imrmnabil@gmail.com>
2023-08-19 22:09:32 +0600

    Add carousel componets

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const el = document.getElementById('event_dropdown')
+    const eventButton = document.getElementById('event_button')
+    eventButton.addEventListener('click', function () {
+        el.classList.toggle('hidden')
+        console.log("hi")
+    })
+}
