d60ace8b8ac9f2c72c8570ee43621b5e1a45846e
Rahat Morshed Nabil <imrmnabil@gmail.com>
2023-08-24 08:35:54 +0600

    Setup localhost server
    Install Express
    Install ejs
    Add index.js
    Rename some files

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', (req, res) => {
+    res.render('home')
+});
+
+app.use(() => {
+    console.log("We have a request")
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
