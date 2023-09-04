368ef86c1c61f0f74be2c7905a9c2e01fb06551d
Rahat Morshed Nabil <imrmnabil@gmail.com>
2023-09-04 11:08:24 +0600

    Define EvenInfos Scema
    Connect EvenInfo Data Collection with Create Event Form

--- index.js
+++ index.js
@@ -26,18 +26,40 @@ const usersInfoSchema = new mongoose.Schema({
       type: String,
       required: true,
       unique: true,
-    },
-    fullname: {
+  },
+  fullname: {
       type: String,
       required: true,
-    },
-  });
+  },
+});
+
+const eventSchema = new mongoose.Schema({
+  event_type: String,
+  event_title: String,
+  startdate: Date,
+  enddate: Date,
+  starttime: String,
+  endtime: String,
+  venue: String,
+  contactmail: String,
+  payment: String,
+  contactphone: String,
+  ticket_price: Number,
+  banner_image:
+  {url: String,
+    filename: String},
+  poster_image:
+    {url: String,
+    filename: String},
+  description: String,
+  additionalinfo: String,
+});
+
 
 // Create the UsersInfo model
 const UsersInfo = mongoose.model('UsersInfo', usersInfoSchema);
+const Event = mongoose.model('EventInfo', eventSchema);
 
-module.exports = UsersInfo; 
-  
 
 
 
@@ -58,13 +80,39 @@ app.get('/create-event', (req, res)=> {
     res.render("create_event");
 })
 
-app.post('/create-event', upload.any() ,(req, res)=> {
+app.post('/create-event', upload.any() , async (req, res)=> {
     try {
         console.log(req.body);
-        console.log(req.files)
-        res.render('create_event')
-        // Your code here
-    } catch (error) {
+        console.log(req.files);
+        console.log(req.body.event_type);
+        const images = req.files.map(f => ({url: f.path, filename: f.filename}))
+        console.log(images[0])
+        const eventData = {
+          event_type: req.body.event_type,
+          event_title: req.body.event_title,
+          startdate: req.body.startdate,
+          enddate: req.body.enddate,
+          starttime: req.body.starttime,
+          endtime: req.body.endtime,
+          venue: req.body.venue,
+          contactmail: req.body.contactmail,
+          payment: req.body.payment,
+          contactphone: req.body.contactphone,
+          ticket_price: req.body.ticket_price,
+          banner_image: images[0], // Assuming you want to store the file name or URL
+          poster_image: images[1], // Assuming you want to store the file name or URL
+          description: req.body.description,
+          additionalinfo: req.body.additionalinfo};
+
+        await console.log(eventData);
+        const newEvent = new Event(eventData);
+        await newEvent.save()
+        .then(() => {
+            console.log("Event Saved!")
+        });
+        await res.render('create_event')
+    } 
+    catch (error) {
         console.error(error);
         res.status(500).send("Error uploading files.");
     }
@@ -76,7 +124,7 @@ app.post('/signup', (req,res)=> {
    newUser.save()
    .then(()=> {
     console.log("User Info saved in mongodb")
-   })
+})
    console.log(uid);
    console.log(name);
 })
